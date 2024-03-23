7a6404f404097aa0a83570ff1650a737b008e7ec
pyrotech52 <coke.pyrotech@gmail.com>
2024-03-22 17:22:56 -0700

    fg

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Install puppet-lint
-exec { 'install flask':
-  command => 'sudo pip3 install flask==2.1.0',
-  path    => '/usr/bin',
-  }
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
