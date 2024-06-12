41141a29e4cf1919431a65f5e5ed927f5e47f8a4
pyrotech <coke.pyrotech@gmail.Com>
2024-06-12 16:01:47 +0100

    pyrotech

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,5 @@
+# Reconfigure the OS for 'holberton' to login and open a file without any error message
+exec {'config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
